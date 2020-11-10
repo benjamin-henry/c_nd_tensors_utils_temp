cd7e91bcb76e58e64e6fd58718a88136080b9542
unknown <benjamin.henry230292@gmail.com>
2020-11-10 17:18:58 +0100

    added nd_to_1d() indexing function

---
+++ nd_to_1d.h
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+
+int nd_to_1d_idx(unsigned int dims, unsigned int* shape, unsigned int* idxs) {
+    // https://stackoverflow.com/questions/29142417/4d-position-from-1d-index
+
+    /*
+    How to use:
+
+        unsigned int index = nd_to_1d_idx(4,(unsigned int[]){2,8,8,3},(unsigned int[]){0,1,2,1});
+
+    OR
+
+        unsigned int dims = 4,
+        unsigned int shape = {2,8,8,3};
+        unsigned int nd_idx = {0,1,2,1};
+        unsigned int index = nd_to_1d_idx(dims, shape, nd_idx); 
+
+    BOTH will return 31
+    */
+    
+    unsigned int* prod_accu = (unsigned int*)(malloc(sizeof(unsigned int) * dims-1));
+    prod_accu[0]=1;
+
+    for(int i=dims-1; i>0; --i) {
+        prod_accu[dims-i]=1;
+        prod_accu[dims-i]=prod_accu[dims-i-1] * shape[i];
+    }
+
+    unsigned int* reversed = (unsigned int*)(malloc(sizeof(unsigned int) * dims-1));
+    for(int i=0; i<dims; ++i) {
+        reversed[i] = prod_accu[dims-i-1];
+    }
+    free(prod_accu);
+    unsigned int index_1d = 0;
+    for(int i=0; i<t->dims; ++i) {
+        index_1d += reversed[i] * idxs[i];
+    }
+    free(reversed);
+    return index_1d;
+}
+
+
+
+
